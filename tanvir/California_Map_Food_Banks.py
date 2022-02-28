d4252ef03fdd5713fac41b756034f60b5c3d5303
Tanvir Hussain <thussain@ucsd.edu>
2022-02-27 19:38:54 -0800

    Update Tanvir's Code (California_Map_Food_Banks)
    Updated Tanvir's Code to fit standard of coding practices and to be consistent with internal project folder.

--- tanvir/California_Map_Food_Banks.py
+++ tanvir/California_Map_Food_Banks.py
@@ -3,13 +3,12 @@ import pgeocode #For converting zip code to longitude/latitude
 import matplotlib.pyplot as plt #for plotting
 from mpl_toolkits.basemap import Basemap
 
-PROJECT_ROOT_DIR = "C:\School\ECE_143\Project"
+#Change to project directory
+PROJECT_ROOT_DIR = "C:\School\ECE_143\ECE_143_Group_Project\\"
 
 #Read in data
-fname = 'C:\School\ECE_143\Project\california_foodbanks.csv'
+fname = PROJECT_ROOT_DIR + 'tanvir\california_foodbanks.csv'
 data = pd.read_csv(fname)
-import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
 
 #Extract zip code
 #Note that some of these only get one zip code in each county may lead to more data on density
@@ -21,7 +20,9 @@ for index, row in data.iterrows():
         zip_codes.append(row['Address'][-10:-5])
     else:
         zip_codes.append(row['Address'][-5:])
-print(zip_codes)
+        
+# #Debugging
+# print(zip_codes)
 
 #Convert zip code to longitude, latitude
 nomi = pgeocode.Nominatim('us')
@@ -29,11 +30,13 @@ query = nomi.query_postal_code(zip_codes)
 coords = {  "lat": query["latitude"],
             "lon": query["longitude"]}
 coordinates = pd.DataFrame.from_dict(coords)
+
+# #Debugging
 # print(query)
 # print(coords)
 # print(coords["lat"][0])
 
-#Purely plotting longitude and latitude histogram style
+# #Purely plotting longitude and latitude histogram style
 # coords["lat"].hist()
 # plt.title("Latitude of Food Banks")
 # plt.show()
@@ -42,14 +45,14 @@ coordinates = pd.DataFrame.from_dict(coords)
 # plt.title("Longitude of Food Banks")
 # plt.show()
 
-#Purely plotting longitude and latitude scatter style
+# #Purely plotting longitude and latitude scatter style
 # coordinates.plot(kind="scatter", x = 'lon', y = 'lat')
 # plt.xlabel('Longitude')
 # plt.ylabel('Latitude')
 # plt.title('Bad superposition of geocoordinates of food banks')
 # plt.show()
 
-#Using random image and not conforming
+# #Using random image and not conforming
 # california_img = mpimg.imread(os.path.join(PROJECT_ROOT_DIR,'prepared_images','california.png'))
 # california_img = mpimg.imread("C:\School\ECE_143\Project\prepared_images\california_county_map.jpg")
 # ax = coordinates.plot(kind="scatter", x="lon", y="lat", figsize=(10,7), label="Food Banks")
@@ -60,8 +63,8 @@ coordinates = pd.DataFrame.from_dict(coords)
 # plt.show()
 
 #Plot coordinates in meaningful way
-lat = coordinates["lat"]
-lon = coordinates["lon"]
+lat = list(coordinates["lat"])
+lon = list(coordinates["lon"])
 fig = plt.figure(figsize=(8, 8))
 m = Basemap(projection='lcc', resolution='c', 
             lat_0=37.5, lon_0=-119,
