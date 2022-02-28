426fec4740520c5d134d2d16c8c12d5f10105642
Tanvir Hussain <thussain@ucsd.edu>
2022-02-27 20:52:02 -0800

    Food Bank Geocoordinates in relation to Food Insecurity Rates
    Fully combined Nick's and Tanvir's Vizualizations

--- tanvir/California_Map.py
+++ tanvir/California_Map.py
@@ -1,14 +1,14 @@
 import pandas as pd #For reading in data
 import pgeocode #For converting zip code to longitude/latitude
 import matplotlib.pyplot as plt #for plotting
-import numpy as np
-from matplotlib import cm
 import plotly.figure_factory as ff
+import matplotlib.image as mpimg
+import os
 
 #Change to project directory
 PROJECT_ROOT_DIR = "C:\School\ECE_143\ECE_143_Group_Project\\"
 
-#Read in data
+#Read in data for geographical coordinate tranformation
 fname = PROJECT_ROOT_DIR + 'Data\california_foodbanks.csv'
 data = pd.read_csv(fname)
 
@@ -35,23 +35,33 @@ coordinates = pd.DataFrame.from_dict(coords)
 
 #Start of Nick's Code (Subject to change)
 
-#Read in data
+#Read in data for heatmap
 fname = PROJECT_ROOT_DIR + 'county food insercuity in CA.csv'
 county_food_inse = pd.read_csv(fname)
 
 #Debugging
-# print(county_food_inse['percent'].mean() + 2 * county_food_inse['percent'].std())
+print(county_food_inse['percent'].mean() + 2 * county_food_inse['percent'].std())
 
+# #Create image in browser
 values = county_food_inse['percent'].tolist()
 fips = county_food_inse['fips'].tolist()
 color = ['#c35b7e' , '#910736', '#866ba8', '#f13710', '#f8c928', '#ff8817']
 
-fig = ff.create_choropleth(
-    fips=fips, values=values, scope=['CA'],
-    binning_endpoints=[6.100608433011778, 9.010649044092098, 11.920689655172417, 14.830730266252736, 17.740770877333055],
-    colorscale=color,
-    county_outline={'color': 'rgb(255,255,255)', 'width': 0.5}, round_legend_values=True,
-    legend_title='Food Insecurity Percentage by County', title='California Food Insecurity Percentage by County'
-)
-fig.layout.template = None
-fig.show()
+# fig = ff.create_choropleth(
+#     fips=fips, values=values, scope=['CA'],
+#     binning_endpoints=[6.100608433011778, 9.010649044092098, 11.920689655172417, 14.830730266252736, 17.740770877333055],
+#     colorscale=color,
+#     county_outline={'color': 'rgb(255,255,255)', 'width': 0.5}, round_legend_values=True,
+#     legend_title='Food Insecurity Percentage by County', title='California Food Insecurity Percentage by County'
+# )
+
+# #Using random image and not conforming
+california_img = mpimg.imread(os.path.join(PROJECT_ROOT_DIR,'Image','food_inse.png'))
+ax = coordinates.plot(kind="scatter", x="lon", y="lat", figsize=(10,7), label="Food Banks")
+#extent = [bottom_left_lat, top_right_lat, bottom_left_log, top_right_lat]
+plt.imshow(california_img, extent=[-131.25, -107.8, 32, 43])
+plt.ylabel("Latitude", fontsize=14)
+plt.xlabel("Longitude", fontsize=14)
+plt.title("Food Bank Geocoordinates in relation to Food Insecurity Rates")
+plt.legend(loc = 'lower right')
+plt.show()
